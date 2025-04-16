ffa45aec3e8a72ad53a7dbb7fd10fe0d6936b987
domyeong <wjsehaud9413@naver.com>
2025-04-17 02:31:42 +0900

    feat: gh-pages 배포 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { loadEnv } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js",
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "src/setupTests.js",
+      },
+      base: env.VITE_BASE_URL,
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.basic.html"),
+            hash: resolve(__dirname, "index.advanced.html"),
+          },
+        },
+      },
+    }),
+  );
+};
